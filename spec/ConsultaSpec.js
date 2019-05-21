32a1220a6a83d0b2742712caabb59ec69362d9e6
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-05-21 20:16:38 -0300

    Criando mais describes para os testes

--- spec/ConsultaSpec.js
+++ spec/ConsultaSpec.js
@@ -6,28 +6,36 @@ describe("Testando consulta", function(){
         paciente = new Paciente("César Júnior", 25, 90, 1.81);
     });
 
-    it("Consulta é retorno", function(){
-        var consulta = new Consulta(paciente, [], false, true);
-        var precoFinal = consulta.preco();
+    describe("Consulta que é retorno", function(){
+        it("Consulta é retorno", function(){
+            var consulta = new Consulta(paciente, [], false, true);
+            var precoFinal = consulta.preco();
 
-        expect(precoFinal).toEqual(0);
+            expect(precoFinal).toEqual(0);
+        });
     });
+
+    describe("Consulta que é procedimento comum", function(){
+        
+        it("Consulta procedimento comum", function(){
+            var consulta = new Consulta(paciente, ['procA', 'procB'], false, false);
+
+            expect(consulta.preco()).toEqual(50);
+        });
+        
+        it("Consulta procedimento comum e particular", function(){
+            var consulta = new Consulta(paciente, ['procA', 'procB'], true, false);
     
-    it("Consulta procedimento comum", function(){
-        var consulta = new Consulta(paciente, ['procA', 'procB'], false, false);
-
-        expect(consulta.preco()).toEqual(50);
+            expect(consulta.preco()).toEqual(100);
+        });
     });
 
-    it("Consulta procedimento comum e particular", function(){
-        var consulta = new Consulta(paciente, ['procA', 'procB'], true, false);
+    describe("Consulta procedimento especial", function(){
+                
+        it("Consulta procedimentos especiais e particular", function(){
+            var consulta = new Consulta(paciente, ['raiox', 'gesso'], true, false);
 
-        expect(consulta.preco()).toEqual(100);
-    });
-    
-    it("Consulta procedimentos especiais e particular", function(){
-        var consulta = new Consulta(paciente, ['raiox', 'gesso'], true, false);
-
-        expect(consulta.preco()).toEqual(134);
+            expect(consulta.preco()).toEqual(134);
+        });
     });
 });
