4a0900f6d09498e657c49a9f045baf96c96d5a82
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-05-20 22:42:27 -0300

    Usando beforeEach para centralizar criação de objeto usado em todos os testes

--- spec/ConsultaSpec.js
+++ spec/ConsultaSpec.js
@@ -1,26 +1,32 @@
 describe("Testando consulta", function(){
 
+    var paciente;
+
+    beforeEach(function(){
+        paciente = new Paciente("César Júnior", 25, 90, 1.81);
+    });
+
     it("Consulta é retorno", function(){
-        var consulta = new Consulta(new Paciente(), [], false, true);
+        var consulta = new Consulta(paciente, [], false, true);
         var precoFinal = consulta.preco();
 
         expect(precoFinal).toEqual(0);
     });
     
     it("Consulta procedimento comum", function(){
-        var consulta = new Consulta(new Paciente(), ['procA', 'procB'], false, false);
+        var consulta = new Consulta(paciente, ['procA', 'procB'], false, false);
 
         expect(consulta.preco()).toEqual(50);
     });
 
     it("Consulta procedimento comum e particular", function(){
-        var consulta = new Consulta(new Paciente(), ['procA', 'procB'], true, false);
+        var consulta = new Consulta(paciente, ['procA', 'procB'], true, false);
 
         expect(consulta.preco()).toEqual(100);
     });
     
     it("Consulta procedimentos especiais e particular", function(){
-        var consulta = new Consulta(new Paciente(), ['raiox', 'gesso'], true, false);
+        var consulta = new Consulta(paciente, ['raiox', 'gesso'], true, false);
 
         expect(consulta.preco()).toEqual(134);
     });
